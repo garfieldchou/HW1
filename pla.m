75c3eae7e96acda80ad0f165d0ff962e4ce30e0c
garfield_chou <garfield_chou@asus.com>
2017-07-14 14:11:58 +0800

    Add x0 for w0 for bug fix

--- pla.m
+++ pla.m
@@ -1,17 +1,17 @@
 clear
 data = dlmread('hw1_15_train.dat');
-X = data(:, [1: 4]);
 y = data(:, 5);
 m = length(y);
+X = [ones(m, 1) , data(:, [1: 4])];
 w = zeros(size(X, 2), 1);
 
 completed = 0;
 update = 0;
 idx = 0;
 
-while (completed < m)
+while completed < m
 
-		n = mod(idx + completed, m)+ 1
+		n = mod(idx + completed, m)+ 1;
 	
 		if X(n, :)* w > 0
 			y_pred = 1;
@@ -20,13 +20,13 @@ while (completed < m)
 		end
 
 		if y(n) != y_pred
-			update += 1
+			update += 1;
 			w = w + y(n)* X(n, :)' ;
-			fprintf('n =%3d, w = [%f %f %f %f]\r\n\n', n, w);
-            idx += completed;
+            idx += (completed + 1);
 			completed = 0;
 			continue;
 		end
-		completed += 1
+		completed += 1;
 	
 end
+fprintf('It took %d updates to complete PLA\r\n', update);
